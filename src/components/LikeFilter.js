25680eb97958927b8cb42cd6ee0cf3134d812240
Yvonne-del <ys359375@gmail.com>
2025-04-23 00:52:33 +0300

    created components

---
+++ src/components/LikeFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LikeFilter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LikeFilter
---
+++ src/components/Likesbutton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Likesbutton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Likesbutton
